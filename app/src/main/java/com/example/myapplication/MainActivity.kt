8c968843c2abf73e56f5e2e4a8797759711c951b
udaysonu <udaysonubakka123@gmail.com>
2020-08-27 15:34:06 +0530

    first commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -34,7 +34,7 @@ class MainActivity : Activity(){
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_maing
+        setContentView(R.layout.activity_main)
 
 
         if(ContextCompat.checkSelfPermission(this,
